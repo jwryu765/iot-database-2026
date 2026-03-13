edc2682d44fa65a5c84bc9d9af5d891a58592a87
jwryu765 <jhryu7658@gmail.com>
2026-03-13 17:32:05 +0900

    1일차

---
+++ day01/1.SELECT기본.sql
@@ -0,0 +1,35 @@
+use madangdb;
+
+-- 한글이 나눔고딕으로 출력됨
+-- 일반 조회 쿼리
+select * from Book;
+
+-- 1. 김연아 고객의 전화를 찾으시오
+select phone from Customer
+where name = '김연아';
+
+-- 한줄 주석
+/* 여러줄
+	주석 */
+-- 모든 쿼리의 마지막은 ; 으로 끝냄. ;은 옵션이지만 무조건 사용권장
+SELECT * FROM Book;
+
+-- 테이블에서 가져오는 데이터가 아니면 FROM 절 생략
+SELECT NOW();
+
+-- 열 명시
+SELECT bookname, price
+   FROM Book;
+
+-- 필터링
+SELECT * FROM Customer
+ WHERE custid > 1 AND custid < 5;
+
+-- 정렬
+SELECT * FROM Book;
+
+SELECT * FROM Book
+ ORDER BY bookid ASC; -- asc 생략가능
+
+SELECT * FROM Book
+ ORDER BY bookid DESC;
